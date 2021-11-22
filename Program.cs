8ce2d2b5b97462d1df6c3c1e34a90c541b4e926b
jeanr0cha <93496120+jeanr0cha@users.noreply.github.com>
2021-11-21 22:49:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*projeto consiste em criar um programa capaz de gerenciar um sistema de loja 
+contendo informações de seus clientes, vendedores, produtos vendidos, comissões e pedidos. */
+
+using System;
+
+namespace Projeto_2_AED_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preojeto 2 AED 1");
+        }
+    }
+}
