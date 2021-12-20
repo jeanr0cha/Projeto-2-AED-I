f171a16963ee2574e1fc3fcd81c6cdd4f50bc02c
jeanr0cha <93496120+jeanr0cha@users.noreply.github.com>
2021-12-19 21:57:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projeto_2_AED_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
---
+++ Vendedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_2_AED_1
+{
+    class Vendedor : Pessoa
+    {
+        private int Loguin; 
+
+        public Vendedor(string nome, long cpf, long telefone, int loguin) : base(nome, cpf, telefone)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Loguin = loguin;
+
+        }
+    }
+}
